b0e1b32b85bf68cc8f67e8a0424c417769970f3c
2018X5zzt <1154322610@qq.com>
2023-11-14 09:54:49 +0800

    [new] 删除了redis和druid的配置，目前能够正常跑通接口

--- backend/part4/elmboot/src/main/java/com/neusoft/elmboot/po/User.java
+++ backend/part4/elmboot/src/main/java/com/neusoft/elmboot/po/User.java
@@ -7,7 +7,6 @@ import lombok.Data;
 @Data
 public class User implements Serializable {
 
-    private static final long serialVersionUID = -3565823820366948318L;
     private String userId;
     private String password;
     private String userName;
