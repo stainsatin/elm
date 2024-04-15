8ca82189ebc9daad48665ce1d577b767fa107d1c
ydt <2352910136@qq.com>
2024-04-15 18:25:40 +0800

    修改了钱包controller

--- ELM-SpringCloud/virtualWallet_server_10900/src/main/java/com/tju/elmcloud/controller/VirtualWalletController.java
+++ ELM-SpringCloud/virtualWallet_server_10900/src/main/java/com/tju/elmcloud/controller/VirtualWalletController.java
@@ -4,6 +4,8 @@ import com.tju.elmcloud.service.VirtualWalletService;
 import com.tju.elmcloud.po.CommonResult;
 import com.tju.elmcloud.po.VirtualWalletVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,30 +13,48 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/VirtualWalletController")
 public class VirtualWalletController {
+
     @Autowired
     VirtualWalletService virtualWalletService;
-    @RequestMapping("/withdrawMoney")
-    public CommonResult<Integer> withdrawMoney(VirtualWalletVo virtualWalletVo) throws Exception{
+    
+    @RequestMapping("/withdrawMoney/{walletId}/{money}")
+    public CommonResult<Integer> withdrawMoney(@PathVariable("walletId") Integer walletId,
+            @PathVariable("money") double money) throws Exception{
         return new CommonResult(200,"success",
-        		virtualWalletService.withdrawMoney(virtualWalletVo.getWalletId(),virtualWalletVo.getMoney()));
+        		virtualWalletService.withdrawMoney(walletId,money));
     }
-    @RequestMapping("/transferMoney")
-    public CommonResult<Integer> transferMoney(VirtualWalletVo virtualWalletVo)throws Exception{
+    @RequestMapping("/transferMoney/{inputWalletId}/{outputWalletId}/{money}/{orderId}")
+    public CommonResult<Integer> transferMoney(@PathVariable("inputWalletId") Integer inputWalletId,
+            @PathVariable("outputWalletId") Integer outputWalletId,
+            @PathVariable("money") double money,
+            @PathVariable("orderId") Integer orderId
+            )throws Exception{
     	return new CommonResult(200,"success", 
-    			virtualWalletService.transferMoney(virtualWalletVo.getInputWalletId(),virtualWalletVo.getOutputWalletId(),virtualWalletVo.getMoney(),virtualWalletVo.getOrderId()));
+    			virtualWalletService.transferMoney(inputWalletId,outputWalletId,money,orderId));
     }
-    @RequestMapping("/recharge")
-    public CommonResult<Integer> recharge(VirtualWalletVo virtualWalletVo)throws Exception{
-    	return new CommonResult(200,"success",  virtualWalletService.recharge(virtualWalletVo.getWalletId(),virtualWalletVo.getMoney()));
-    }
-
-    @RequestMapping("/userCreateVirtualWallet")
-    public CommonResult<Integer> userCreateVirtualWallet(VirtualWalletVo virtualWalletVo)throws Exception{
-    	return new CommonResult(200,"success",  virtualWalletService.userCreateVirtualWallet(virtualWalletVo.getUserId()));
+    @RequestMapping("/recharge/{walletId}/{money}")
+    public CommonResult<Integer> recharge(@PathVariable("walletId") Integer walletId,
+            @PathVariable("money") double money)throws Exception{
+    	return new CommonResult(200,"success",  virtualWalletService.recharge(walletId,money));
     }
 
-    @RequestMapping("/queryBalance")
-    public CommonResult<Double> queryBalance(VirtualWalletVo virtualWalletVo) throws Exception{
-    	return new CommonResult(200,"success", virtualWalletService.queryBalance(virtualWalletVo.getWalletId()));
+    @RequestMapping("/userCreateVirtualWallet/{userId}")
+    public CommonResult<Integer> userCreateVirtualWallet(@PathVariable("userId") String userId)throws Exception{
+    	return new CommonResult(200,"success",  virtualWalletService.userCreateVirtualWallet(userId));
+    }
+
+    @RequestMapping("/queryBalance/{walletId}")
+    public CommonResult<Double> queryBalance(@PathVariable("walletId") Integer walletId) throws Exception{
+    	return new CommonResult(200,"success", virtualWalletService.queryBalance(walletId));
+    }
+    
+    @RequestMapping("/queryBalance/{inputWalletId}/{outputWalletId}/{money1}/{money2}/{orderId}")
+    public CommonResult<Integer> transferMoneyWithCredit(@PathVariable("inputWalletId") Integer inputWalletId,
+            @PathVariable("outputWalletId") Integer outputWalletId,
+            @PathVariable("money1") double money1,
+            @PathVariable("money2") double money2,
+            @PathVariable("orderId") Integer orderId
+            ) throws Exception{
+    	return new CommonResult(200,"success", virtualWalletService.transferMoneyWithCredit(inputWalletId, outputWalletId, money1, money2, orderId));
     }
 }
